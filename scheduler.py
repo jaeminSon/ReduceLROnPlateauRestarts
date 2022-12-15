2b22a2b44ed065fe6526bda5a600afdee6dfd5d3
jaeminSon <woalsdnd@gmail.com>
2022-12-15 23:00:41 +0900

    inherit torch.optim.lr_scheduler.ReduceLROnPlateau

--- scheduler.py
+++ scheduler.py
@@ -1,61 +1,79 @@
-class ReduceLROnPlateauAnnealing(object):
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+class ReduceLROnPlateauAnnealing(ReduceLROnPlateau):
+    """Reduce learning rate when a metric has stopped improving.
+    Models often benefit from reducing the learning rate by a factor
+    of 2-10 once learning stagnates. This scheduler reads a metrics
+    quantity and if no improvement is seen for a 'patience' number
+    of epochs, the learning rate is reduced.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        min_lr (float or list): A scalar or a list of scalars. A
+            lower bound on the learning rate of all param groups
+            or each group respectively.
+        max_lr (float or list): A scalar or a list of scalars. An 
+            upper bound on the learning rate of all param groups
+            or each group respectively. Default: learnig rate of optimizer.
+        mode (str): One of `min`, `max`. In `min` mode, lr will
+            be reduced when the quantity monitored has stopped
+            decreasing; in `max` mode it will be reduced when the
+            quantity monitored has stopped increasing. Default: 'min'.
+        factor (float): Factor by which the learning rate will be
+            reduced. new_lr = lr * factor. Default: 0.1.
+        patience (int): Number of epochs with no improvement after
+            which learning rate will be reduced. For example, if
+            `patience = 2`, then we will ignore the first 2 epochs
+            with no improvement, and will only decrease the LR after the
+            3rd epoch if the loss still hasn't improved then.
+            Default: 10.
+        threshold (float): Threshold for measuring the new optimum,
+            to only focus on significant changes. Default: 1e-4.
+        threshold_mode (str): One of `rel`, `abs`. In `rel` mode,
+            dynamic_threshold = best * ( 1 + threshold ) in 'max'
+            mode or best * ( 1 - threshold ) in `min` mode.
+            In `abs` mode, dynamic_threshold = best + threshold in
+            `max` mode or best - threshold in `min` mode. Default: 'rel'.
+        cooldown (int): Number of epochs to wait before resuming
+            normal operation after lr has been reduced. Default: 0.
+        eps (float): Minimal decay applied to lr. If the difference
+            between new and old lr is smaller than eps, the update is
+            ignored. Default: 1e-8.
+        verbose (bool): If ``True``, prints a message to stdout for
+            each update. Default: ``False``.
+
+    Example:
+        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+        >>> scheduler = ReduceLROnPlateau(optimizer, 'min')
+        >>> for epoch in range(10):
+        >>>     train(...)
+        >>>     val_loss = validate(...)
+        >>>     # Note that step should be called after validate()
+        >>>     scheduler.step(val_loss)
     """
-        lr = baselr * 0.1 if validation measure plateaus (measure does not increase)
-        lr = init_lr if lr < min_lr
-    """
+    def __init__(self, optimizer, min_lr, max_lr=None, mode='min', factor=0.1, patience=10,
+                 threshold=1e-4, threshold_mode='rel', cooldown=0,
+                 eps=1e-8, verbose=False):
+        super(ReduceLROnPlateauAnnealing, self).__init__(optimizer, mode=mode, factor=factor, patience=patience,
+                                                threshold=threshold, threshold_mode=threshold_mode, cooldown=cooldown,
+                                                min_lr=min_lr, eps=eps, verbose=verbose)
 
-    def __init__(self, init_lr, min_lr, n_epochs_plateau, decay_factor=0.1) -> None:
+        if max_lr is None:
+            self.max_lrs = [p['lr'] for p in optimizer.param_groups]
+        elif isinstance(max_lr, list) or isinstance(max_lr, tuple):
+            if len(max_lr) != len(optimizer.param_groups):
+                raise ValueError("expected {} max_lrs, got {}".format(
+                    len(optimizer.param_groups), len(max_lr)))
+            self.max_lrs = list(max_lr)
+        else:
+            self.max_lrs = [max_lr] * len(optimizer.param_groups)
 
-        self.lr = init_lr
-        self.n_epochs_plateau = n_epochs_plateau
-        self.n_epochs_no_updates = 0
-        self.epochs = -1
-        self.min_lr = min_lr
-        self.init_lr = init_lr
-        self.decay_factor = decay_factor
-        self.best_measure = -float("inf")
-
-    def __call__(self, optimizer, epoch, curr_measure) -> None:
-        if self.epochs != epoch:
-            self.epochs = epoch
-            if self.n_epochs_plateau <= self.n_epochs_no_updates:
-                self.n_epochs_no_updates = 0
-                if self.lr * self.decay_factor < self.min_lr:
-                    self.lr = self.init_lr
-                else:
-                    self.lr *= self.decay_factor
-            else:
-                if self.best_measure < curr_measure:
-                    self.best_measure = curr_measure
-                    self.n_epochs_no_updates = 0
-                else:
-                    self.n_epochs_no_updates += 1
-            print("best:{}, curr:{}, n_epochs_no_updates: {}, lr: {}".format(
-                self.best_measure, curr_measure, self.n_epochs_no_updates, self.lr))
-        self._adjust_learning_rate(optimizer)
-
-    def _adjust_learning_rate(self, optimizer) -> None:
-        assert 0 < self.lr
-        for i in range(len(optimizer.param_groups)):
-            optimizer.param_groups[i]['lr'] = self.lr
-
-if __name__ == "__main__":
-
-    import torch
-
-    init_lr = 1e-4
-    min_lr = 1e-7
-    n_epochs_plateau = 10
-    
-    total_epochs = 100
-
-    optimizer = torch.optim.AdamW(network.parameters(), init_lr)
-
-    lr_scheduler = ReduceLROnPlateauAnnealing(init_lr, min_lr, n_epochs_plateau, decay_factor=0.1)
-
-    for epoch in range(total_epochs):
-        # train & val loops & save checkpoint
-        
-        # update learning rate scheduler
-        lr_scheduler(optimizer, epoch, val_measure)
-        
+    def _reduce_lr(self, epoch):
+        for i, param_group in enumerate(self.optimizer.param_groups):
+            old_lr = float(param_group['lr'])
+            lr_cand = old_lr * self.factor
+            new_lr = self.max_lrs[i] if abs(lr_cand - self.min_lrs[i]) < self.eps else lr_cand
+            param_group['lr'] = new_lr
+            if self.verbose:
+                print('Epoch {:5d}: reducing learning rate'
+                        ' of group {} to {:.4e}.'.format(epoch, i, new_lr))
