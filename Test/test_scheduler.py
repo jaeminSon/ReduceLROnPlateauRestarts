8c7336ab8c0cda1b128c1e634feb0b4a601eae31
jaeminSon <woalsdnd@gmail.com>
2023-01-06 14:10:18 +0900

    test lr given as list

--- Test/test_scheduler.py
+++ Test/test_scheduler.py
@@ -4,8 +4,43 @@ from scheduler import ReduceLROnPlateauAnnealing
 
 
 class Test:
-    
-    @pytest.mark.parametrize("min_lr,max_lr,patience", [(1e-7,1e-4,5)])
+    @pytest.mark.parametrize("min_lr,max_lr,patience", [(5e-7,1e-4,5)])
+    def test_steady_val_measure_lr_list(self, min_lr, max_lr, patience):
+        
+        import torch
+        from torchvision.models import resnet18
+
+        optimizer = torch.optim.AdamW(resnet18().parameters(), max_lr)
+        n_param_groups = len(optimizer.param_groups)
+        lr_scheduler = ReduceLROnPlateauAnnealing(optimizer, [min_lr]*n_param_groups, [max_lr]*n_param_groups, patience=patience)
+        
+        list_lr = []
+        for _ in range(100):
+            lr_scheduler.step(0)
+            list_lr.append(lr_scheduler.optimizer.param_groups[0]['lr'])
+
+        expected = [max_lr*(0.1)**((i//6)%3) for i in range(100)] # [1e-4]*6 + [1e-5]*6 + [1e-6]*6 + [1e-4]*6
+        assert all([abs(list_lr[i]-expected[i]) < 1e-8 for i in range(100)])
+
+    @pytest.mark.parametrize("min_lr,max_lr,patience", [(5e-7,1e-4,5)])
+    def test_improving_val_measure_lr_list(self, min_lr, max_lr, patience):
+        
+        import torch
+        from torchvision.models import resnet18
+
+        optimizer = torch.optim.AdamW(resnet18().parameters(), max_lr)
+        n_param_groups = len(optimizer.param_groups)
+        lr_scheduler = ReduceLROnPlateauAnnealing(torch.optim.AdamW(resnet18().parameters(), max_lr), [min_lr]*n_param_groups, [max_lr]*n_param_groups, patience=patience)
+
+        list_lr = []
+        for i in range(100):
+            lr_scheduler.step(-i)
+            list_lr.append(lr_scheduler.optimizer.param_groups[0]['lr'])
+
+        expected = [max_lr] * 100 # [1e-4]*100
+        assert all([abs(list_lr[i]-expected[i]) < 1e-8 for i in range(100)])
+
+    @pytest.mark.parametrize("min_lr,max_lr,patience", [(5e-7,1e-4,5)])
     def test_steady_val_measure(self, min_lr, max_lr, patience):
         
         import torch
@@ -21,7 +56,7 @@ class Test:
         expected = [max_lr*(0.1)**((i//6)%3) for i in range(100)] # [1e-4]*6 + [1e-5]*6 + [1e-6]*6 + [1e-4]*6
         assert all([abs(list_lr[i]-expected[i]) < 1e-8 for i in range(100)])
 
-    @pytest.mark.parametrize("min_lr,max_lr,patience", [(1e-7,1e-4,5)])
+    @pytest.mark.parametrize("min_lr,max_lr,patience", [(5e-7,1e-4,5)])
     def test_improving_val_measure(self, min_lr, max_lr, patience):
         
         import torch
